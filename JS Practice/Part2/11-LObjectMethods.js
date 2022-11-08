53dd044e717467b139a0465db008c3dd8b2488fc
KONETI-JAYANTHVARMA <jayanthvarma09062001@gmail.com>
2022-11-09 00:06:14 +0530

    files created on 09/11/2022

--- JS Practice/Part2/11-LObjectMethods.js
+++ JS Practice/Part2/11-LObjectMethods.js
@@ -18,6 +18,12 @@ const datas={
     calcAge: function(){
         this.age=2022-this.dob;
         return datas.age;
+    },
+
+    sendSummary : function(){
+        this.summary= `${this.firstname} is a ${this.calcAge()} old ${this.Designation}, and he has ${this.hasDriversLicense? 'a':'no'} driver's license`;
+        console.log(this);
+        return datas.summary;
     }
 };
 
@@ -28,3 +34,5 @@ console.log(datas.calcAge());
 console.log(datas.calcAge());
 // console.log(datas.calcAge(2001))
 //console.log(datas['calcAge'](2001));
+
+console.log(datas.sendSummary());
